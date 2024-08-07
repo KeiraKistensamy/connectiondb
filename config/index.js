172dce9a4fd47c3556fbc2fd8ec228c0f8f3b438
KeiraKistensamy <Keirakist0987@gmail.com>
2024-08-07 15:21:49 +0200

    connectiondb

---
+++ config/index.js
@@ -0,0 +1,19 @@
+import { createPool } from "mysql2";
+import 'dotenv/config.js'
+
+let connection = createPool ({
+    host: process.env.hostDb,
+    user: process.env.userDb,
+    password:  process.env.pwdDb,
+    database: process.env.dbName,
+    multipleStatement: true,
+    connectionLimit: 30
+})
+
+connection.on('connection', (err) => {
+    if (err) throw new Error('Can not connect to database right now ')
+})
+
+export {
+    connection
+}
